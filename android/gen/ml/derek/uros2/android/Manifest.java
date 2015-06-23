3f51207173ed0e0c5f0ee2006b8f826b6ed847fe
Derek <zorfie123@gmail.com>
2015-06-23 12:41:23 +0100

    Added android project

---
+++ android/gen/ml/derek/uros2/android/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package ml.derek.uros2.android;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
---
+++ android/src/ml/derek/uros2/android/MyActivity.java
@@ -0,0 +1,17 @@
+package ml.derek.uros2.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
