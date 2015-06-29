66f57bbfc98accd7c251436fe2778b6a38379118
Derek <zorfie123@gmail.com>
2015-06-29 17:39:45 +0100

    Removed redudented commented out code

--- desktop/src/main/java/ml/derek/uros2/desktop/Test.java
+++ desktop/src/main/java/ml/derek/uros2/desktop/Test.java
@@ -24,47 +24,13 @@ public class Test
         File file = new File("simple/close1.jpg");
         BufferedImage image = ImageIO.read(file);
         Mat door = Convert.mat(image);
-        //Mat door2 = door.clone();
-
-        //Mat door = Imgcodecs.imread("simple/close1.jpg");
-
-        //Imgproc.cvtColor(door2, door2, Imgproc.COLOR_GRAY2BGR);
-
         List<Line> lineList =  Detection.imageLines(door);
         List<Point> intersections = Detection.lineIntersections(lineList);
-        //lineList = Detection.getDoor(lineList);
 
         Mat doorLines = Draw.lines(lineList, door);
         doorLines = Draw.corners(intersections, doorLines);
-        //List<MatOfPoint> contours = Detection.doorContours(door);
-        //List<Rect> rects = Detection.getBounds(contours);
-        //List<MatOfPoint> hulls = Detection.convexHulls(contours);
 
-
-        //Mat corners = Detection.getCorners(door);
-
-        Point[] corners2 = Detection.getCorners2(door).toArray();
-        //List<Line> lines = Operations.joinPoints(corners2);
-        //List<Line> lines2 = Operations.compareLines(lines);
-        //List<Point> pointList = Convert.pointList(corners);
-        //Operations.mergeAllPoints(pointList);
-
-        //Mat contourMat = Draw.contours(contours, new Mat(door.size(), door.type()));
-        //Mat rectangle = Draw.rect(Detection.largestRect(rects), door);
-        //Mat hullMat = Draw.convexHulls(hulls, door);
-        Mat cornerMat = Draw.corners(corners2, door);
-        //Mat lineMat = Draw.lines(lines, door);
-        //Mat lineMat2 = Draw.lines(lines2, door);
-
-        //BufferedImage doorLinesImage = Convert.bufferedImage(imageLines);
-        //BufferedImage contourImage = Convert.bufferedImage(contourMat);
-        // BufferedImage rectImage = Convert.bufferedImage(rectangle);
-        BufferedImage cornerImage = Convert.bufferedImage(cornerMat);
         BufferedImage lineImage = Convert.bufferedImage(doorLines);
-        //BufferedImage lineImage2 = Convert.bufferedImage(lineMat2);
-        //Imgcodecs.imwrite("compiled.jpg", door2);
-        //BufferedImage door2Image = ImageIO.read(new File("compiled.jpg"));
-
 
         JFrame frame = Display.image(lineImage);
     }
