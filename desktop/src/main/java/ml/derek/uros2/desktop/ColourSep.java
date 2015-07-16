4e6c6d2fe6ceb32da60393df2338e6ed108432c9
Derek <zorfie123@gmail.com>
2015-07-16 10:36:13 +0100

    Added colour seperation algorthim, no idea if it works or not

---
+++ desktop/src/main/java/ml/derek/uros2/desktop/ColourSep.java
@@ -0,0 +1,41 @@
+package ml.derek.uros2.desktop;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.TermCriteria;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColourSep
+{
+    public static Mat seperateColours(Mat image, int numberOfColours)
+    {
+        // Split the image into it's different channels, so the size
+        // of the list should be 3 with each mat successive mat being
+        // one of R, G, B
+        List<Mat> imgRGB = new ArrayList<>();
+        Core.split(image, imgRGB);
+
+        // Create a mat where each row is a set of RGB (?)
+        int k = 5, n = image.rows() * image.cols();
+        Mat img3xN = new Mat(n, 3, CvType.CV_8U);
+
+        // Magical stuff happens here, fear not weary traveller
+        for(int i = 0; i <= 3; i++)
+            imgRGB.get(i).reshape(1, n).copyTo(img3xN.col(i));
+
+        img3xN.convertTo(img3xN, CvType.CV_32F);
+        Mat bestLabels = new Mat();
+
+        // Do the real business right here
+        Core.kmeans(img3xN, k, bestLabels, new TermCriteria(), 10, Core.KMEANS_RANDOM_CENTERS);
+
+        // Convert the result back
+        bestLabels = bestLabels.reshape(0, image.rows());
+        Core.convertScaleAbs(bestLabels, bestLabels, 255 / k, 0);
+        return bestLabels;
+    }
+}
